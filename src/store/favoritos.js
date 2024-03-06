d30dc6673cd141da819833b24215d350b9ce34c9
Marcelino <marcelinomebe@gmail.com>
2024-03-06 18:47:56 -0500

    Guardando el localStorage la informacion de favorito

--- src/store/favoritos.js
+++ src/store/favoritos.js
@@ -3,15 +3,22 @@ import { ref } from "vue";
 
 export const useFavoritosStore = defineStore('favoritos', () => {
     const favoritos = ref([]);
+
+    if (localStorage.getItem('favoritos')) {
+        favoritos.value = JSON.parse(localStorage.getItem('favoritos'));
+    }
+
     const agregarFavorito = (poke) => {
         if(!favoritos.value.includes(poke)){
             favoritos.value.push(poke)
             console.log(favoritos.value);
+            localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
         }
     }
 
     const eliminarFavorito = (id) => {
         favoritos.value = favoritos.value.filter(poke => poke.id !== id);
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
     }
 
     const findPoke = (name) => favoritos.value.find(poke => poke.name === name);
