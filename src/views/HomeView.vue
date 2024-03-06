1e7c3ecc34949a18466a8badd33a50558fa3e97c
Marcelino <marcelinomebe@gmail.com>
2024-03-06 17:32:10 -0500

    usando la desestructuración de objetos  para que el codigo quede mas legible

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,21 @@
 <script setup>
 import { useCounterStore } from '@/store/counter'
+import { storeToRefs } from 'pinia'
 
 const useCounter = useCounterStore()
+//usando la desestructuración de objetos 
+const { increment } = useCounter 
+//usando storeToRefs datos reactivos y computados
+const { count, double } = storeToRefs(useCounter)
 </script>
 
 <template>
   <main>
-    <h1>Home Counter: {{ useCounter.count }}</h1>
-    <h2>Double: {{ useCounter.double }}</h2>
+    <h1>Home Counter: {{ count }}</h1>
+    <h2>Double: {{ double }}</h2>
     <button 
       class="btn btn-outline-primary"
-      @click="useCounter.increment"
+      @click="increment"
     >Increments</button>
   </main>
 </template>
